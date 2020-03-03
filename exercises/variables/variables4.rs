0c3461862f4c6606abbc309814538213121499b5
Dillon Curry <dilloncurry@gmail.com>
2020-03-02 19:52:42 -0500

    Functions completed

--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -1,7 +1,7 @@
 // variables4.rs
 // Make me compile! Execute the command `rustlings hint variables4` if you want a hint :)
 
-wfn main() {
+fn main() {
     let x: i32 = 0;
     println!("Number {}", x);
 }
